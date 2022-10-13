a8735546ad2f15946f777b07697b836611b759b0
Vladimir <vladimir.burshnev@duallab.com>
2022-10-13 14:30:37 +0300

    Wrong bounding box for glyphs bug fix

--- src/core/bounding_boxes.js
+++ src/core/bounding_boxes.js
@@ -50,19 +50,22 @@ var BoundingBoxesCalculator = (function PartialEvaluatorClosure() {
       let ascent = (this.textStateManager.state.font.ascent || 1) * this.textStateManager.state.fontSize;
       let rise = this.textStateManager.state.textRise * this.textStateManager.state.fontSize;
 
-      //Calculate transformed height and shift to place whole glyph inside of bbox
-      let shift = Util.applyTransform([0, descent + rise], this.textStateManager.state.textMatrix);
-      shift[0] -= this.textStateManager.state.textMatrix[4];
-      shift[1] -= this.textStateManager.state.textMatrix[5];
-
-      let height = Util.applyTransform([0, ascent + rise], this.textStateManager.state.textMatrix);
-      height[0] -= this.textStateManager.state.textMatrix[4] + shift[0];
-      height[1] -= this.textStateManager.state.textMatrix[5] + shift[1];
-      height = Math.sqrt(height[0] * height[0] + height[1] * height[1]);
-
       //Left Bottom point of text bbox
       //Save before text matrix will be changed with going through glyphs
-      let [tx0, ty0] = [this.textStateManager.state.textMatrix[4] + shift[0], this.textStateManager.state.textMatrix[5] + shift[1]];
+      let [tx0, ty0] = Util.applyTransform([0, descent + rise], this.textStateManager.state.textMatrix);
+
+      //Calculate transformed height and shift to place whole glyph inside of bbox
+      let shift = [tx0 - this.textStateManager.state.textMatrix[4], ty0 - this.textStateManager.state.textMatrix[5]];
+
+      let height = Util.applyTransform([0, ascent - descent], this.textStateManager.state.textMatrix);
+      height[0] -= this.textStateManager.state.textMatrix[4];
+      height[1] -= this.textStateManager.state.textMatrix[5];
+      height = Math.sqrt(height[0] * height[0] + height[1] * height[1]);
+      if (this.textStateManager.state.font.vertical) {
+        height *= this.textStateManager.state.textMatrix[0] < 0 && this.textStateManager.state.textMatrix[3] >= 0 ? -1 : 1;
+      } else {
+        height *= this.textStateManager.state.textMatrix[3] < 0 && this.textStateManager.state.textMatrix[0] >= 0 ? -1 : 1;
+      }
 
       let glyphsSize = [];
       for (let i = 0; i < glyphs.length; i++) {
@@ -101,14 +104,6 @@ var BoundingBoxesCalculator = (function PartialEvaluatorClosure() {
       //Top point can be calculated from base and height
       let [tx2, ty2, tx3, ty3] = this.getTopPoints(tx0, ty0, tx1, ty1, height);
       glyphsSize = glyphsSize.map(glyphSize => [...glyphSize, ...this.getTopPoints(...glyphSize, height)])
-      if (this.textStateManager.state.textMatrix[3] < 0) {
-        ty0 += height * this.textStateManager.state.textMatrix[3];
-        ty1 += height * this.textStateManager.state.textMatrix[3];
-        ty2 += height * this.textStateManager.state.textMatrix[3];
-        ty3 += height * this.textStateManager.state.textMatrix[3];
-        glyphsSize = glyphsSize.map(glyphSize => [...glyphSize.map((point, index) =>
-          index % 2 === 0 ? point : (point + height * this.textStateManager.state.textMatrix[3]))]);
-      }
 
       //Apply transform matrix to bbox
       let [x0, y0] = Util.applyTransform([tx0, ty0], ctm);
